00a32a971a75c94df03120cf3731a9084708e1c2
Andy Loftus <aloftus@gmail.com>
2016-01-29 11:59:22 -0600

    Updated for multiple src dirs

--- test/clean_test_data.sh
+++ test/clean_test_data.sh
@@ -1,18 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
 function parallel_del_dir() {
     deldir=$1
     mmdepth=$2
-#    find "$deldir" -maxdepth $mmdepth -mindepth $mmdepth -print \
-#    | wc -l
     find "$deldir" -maxdepth $mmdepth -mindepth $mmdepth -print0 \
     | xargs -0 -n1 -P64 -I{} find {} -delete
     find "$deldir" -delete
 }
 
 TODAY=$( date +%s)
-
-srcdir=/projects/test/aloftus/hometest
+srcdirlist=( $( find /projects/test/aloftus -mindepth 1 -maxdepth 1 -type d ) )
 tmpdir=/mnt/b/__PSYNCTMPDIR__
 
 # Setup mv target
@@ -21,14 +18,12 @@ tgtdir="${tgtbase}/${TODAY}"
 mkdir -p $tgtdir
 
 # Move sources out of the way
-for d in "$srcdir" "$tmpdir"; do
-#    set -x
+for d in "${srcdirlist[@]}" "${tmpdir}"; do
+    set -x
     if [[ -d "$d" ]] ; then
         mv "$d" "$tgtdir"/.
-        mkdir -p "$d"
-#        ls -lF "$d"
     fi
-#    set +x
+    set +x
 done
 
 # Iterate over all date-subdirs in tgtbase
@@ -38,18 +33,14 @@ for newbase in $( find "$tgtbase" -mindepth 1 -maxdepth 1 ); do
     set -x
     time parallel_del_dir "$deldir" 1
     set +x
-    # Delete home's
-    src=$( basename $srcdir )
-    case $src in
-        hometest) deldir="${newbase}/$src"
-                  set -x
-                  time parallel_del_dir "$deldir" 1
-                  set +x
-                  ;;
-               *) echo "Error: unknown src '$src' from srcdir '$srcdir'" 1>&2
-                  exit 99
-                  ;;
-    esac
+    # Delete other sources
+    for s in "${srcdirlist[@]}"; do
+        src=$( basename $s )
+        deldir="${newbase}/$src"
+        set -x
+        time parallel_del_dir "$deldir" 1
+        set +x
+    # Delete top dir
     time find "$newbase" -delete
 
 echo "Total Elapsed Seconds: $SECONDS"
