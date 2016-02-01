867bc7b236c63e79a4b3fdca9f15d8d7c86caef2
Andy Loftus <aloftus@gmail.com>
2016-02-01 14:06:13 -0600

    Added --unpause option

--- scripts/workers_pause.py
+++ scripts/workers_pause.py
@@ -2,27 +2,32 @@ import psync
 import argparse
 import pprint
 
-default_queue_name = 'celery'
-parser = argparse.ArgumentParser(
-    description='Tell all workers to stop consuming from the specified queue.' )
-parser.add_argument( '--discard_reserved', '-r', action='store_true',
-    help='Also discard reserved tasks (default=%(default)s)' )
-parser.add_argument( '--discard_waiting', '-w', action='store_true',
-    help='Also discard waiting tasks from queue (default=%(default)s)' )
-parser.add_argument( '--kill_active', '-k', action='store_true',
-    help='Also active tasks that are currently running (default=%(default)s)' )
-parser.add_argument( '--verbose', '-v', action='store_true' )
-parser.add_argument( 'queuenames', nargs=argparse.REMAINDER,
-    help='Queue name(s) to stop consuming from. (default={0})'.format( 
-        default_queue_name ) )
-parser.set_defaults(
-    discard_reserved=False,
-    discard_waiting=False,
-    verbose=False,
-    )
-args = parser.parse_args()
-if len( args.queuenames ) < 1:
-    args.queuenames = [ default_queue_name ]
+def process_cmdline():
+    default_queue_name = 'celery'
+    parser = argparse.ArgumentParser(
+        description='Tell all workers to stop consuming from the specified queue.' )
+    parser.add_argument( '--discard_reserved', '-r', action='store_true',
+        help='Also discard reserved tasks (default=%(default)s)' )
+    parser.add_argument( '--discard_waiting', '-w', action='store_true',
+        help='Also discard waiting tasks from queue (default=%(default)s)' )
+    parser.add_argument( '--kill_active', '-k', action='store_true',
+        help='Also active tasks that are currently running (default=%(default)s)' )
+    parser.add_argument( '--unpause', '-u', action='store_true',
+        help='Unpause tasks. (default=%(default)s)' )
+    parser.add_argument( '--verbose', '-v', action='store_true' )
+    parser.add_argument( 'queuenames', nargs=argparse.REMAINDER,
+        help='Queue name(s) to stop consuming from. (default={0})'.format( 
+            default_queue_name ) )
+#    parser.set_defaults(
+#        discard_reserved=False,
+#        discard_waiting=False,
+#        verbose=False,
+#        unpause=False,
+#        )
+    args = parser.parse_args()
+    if len( args.queuenames ) < 1:
+        args.queuenames = [ default_queue_name ]
+    return args
 
 
 def process_results( results ):
@@ -33,14 +38,25 @@ def process_results( results ):
     return data
     
 
-outfmt = '{name:25} {rv}'
-for q in args.queuenames:
-    results = psync.app.control.cancel_consumer( q, reply=True )
-    result_data_by_worker = process_results( results )
-    for k in sorted( result_data_by_worker ):
-        print( outfmt.format( name=k, rv=str( result_data_by_worker[ k ] ) ) )
+def do_unpause( args ):
+    outfmt = '{name:25} {rv}'
+    for q in args.queuenames:
+        results = psync.app.control.add_consumer( q, reply=True )
+        result_data_by_worker = process_results( results )
+        for k in sorted( result_data_by_worker ):
+            print( outfmt.format( name=k, rv=str( result_data_by_worker[ k ] ) ) )
 
-if args.discard_reserved:
+
+def do_pause( args ):
+    outfmt = '{name:25} {rv}'
+    for q in args.queuenames:
+        results = psync.app.control.cancel_consumer( q, reply=True )
+        result_data_by_worker = process_results( results )
+        for k in sorted( result_data_by_worker ):
+            print( outfmt.format( name=k, rv=str( result_data_by_worker[ k ] ) ) )
+
+
+def discard_reserved( args ):
     total_num_reserved = 0
     inspect = psync.app.control.inspect()
     reserved = inspect.reserved()
@@ -59,7 +75,7 @@ if args.discard_reserved:
     print( '\nRevoked {0} reserved tasks in total\n'.format( total_num_reserved ) )
 
 
-if args.kill_active:
+def kill_active( args ):
     total_num_active = 0
     inspect = psync.app.control.inspect()
     active = inspect.active()
@@ -79,10 +95,21 @@ if args.kill_active:
     print( '\nKilled {0} active tasks in total\n'.format( total_num_active ) )
             
 
-
-if args.discard_waiting:
+def discard_waiting( args ):
     num_waiting = psync.app.control.discard_all()
     print( 'Discarded {0} waiting tasks'.format( num_waiting ) )
+
+
+if __name__ == '__main__':
+    args = process_cmdline()
+    if args.unpause:
+        do_unpause( args )
+    else:
+        do_pause( args )
+        if args.discard_reserved:
+            do_discard_reserved( args )
+        if args.kill_active:
+            do_kill_active( args )
 
 
 
