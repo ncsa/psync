eb934a5ab3c400a5463133eca8080a1296d2d97f
Andy Loftus <aloftus@gmail.com>
2016-02-05 09:00:53 -0600

    Added "Estimated time remaining"

--- test/parse_psync_infolog.py
+++ test/parse_psync_infolog.py
@@ -102,6 +102,7 @@ def print_psync_summary( args, time_data, sync_types, total_rec_count ):
             total_inodes += v[ 'end' ]
     pct_complete_by_inodes = total_inodes * 100.0 / args.inodes
     pct_rate = pct_complete_by_inodes / elapsed.total_seconds() * 3600
+    eta_complete = ( 100.0 - pct_complete_by_inodes ) / pct_rate
     print( 
         'Record counts: {rc}\n'
         'Total log record count: {tlrc}\n'
@@ -110,7 +111,8 @@ def print_psync_summary( args, time_data, sync_types, total_rec_count ):
         'End time: {et_ts} ({et})\n'
         'Elapsed Time: {el}\n'
         'Percent Complete: {pct_c:4.2f}\n'
-        'Percent rate (per Hour): {pct_ph:4.2f}\n'.format( 
+        'Percent rate (per Hour): {pct_ph:4.2f}\n'
+        'Estimated time remaining (hours): {eta:4.2f}\n'.format( 
         rc = pprint.pformat( sync_types ),
         tlrc = total_rec_count,
         icnt = total_inodes,
@@ -120,7 +122,8 @@ def print_psync_summary( args, time_data, sync_types, total_rec_count ):
         et = str( end_time ),
         el = str( elapsed ),
         pct_c = pct_complete_by_inodes,
-        pct_ph = pct_rate ) )
+        pct_ph = pct_rate,
+        eta = eta_complete ) )
 
 
 def print_syncdir_summary( args, syncdir_data ):
