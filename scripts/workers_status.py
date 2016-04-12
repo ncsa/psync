17d16905b8f675bd949e308d198ae7ce48e65e32
Andy Loftus <aloftus@gmail.com>
2016-04-12 12:50:56 -0500

    re-enable inqueue statistics reporting

--- scripts/workers_status.py
+++ scripts/workers_status.py
@@ -22,38 +22,42 @@ parser.add_argument( '--activedetails', '-d', action='store_true',
 #parser.set_defaults( ignoreidle=False )
 args = parser.parse_args()
 
+I = psync.app.control.inspect()
+
 # get per worker stats
-worker_stats = psync.app.control.inspect().stats()
+worker_stats = I.stats()
 if worker_stats is None:
     sys.exit( 'No active workers found.' )
-for worker, stats in psync.app.control.inspect().stats().iteritems():
+for worker, stats in worker_stats.iteritems():
     data[ worker ] = dict( STATS=stats )
 
 # get per worker active tasks
-for worker, activelist in psync.app.control.inspect().active().iteritems():
+for worker, activelist in I.active().iteritems():
     data[ worker ][ 'ACTIVELIST' ] = activelist
 
 # get per worker reserved tasks
-for worker, reservedlist in psync.app.control.inspect().reserved().iteritems():
+for worker, reservedlist in I.reserved().iteritems():
     data[ worker][ 'RESERVEDLIST' ] = reservedlist
 
 # print summary
-#fmt = '{name:15} {maxP:>9} {curP:>9} {inQa:>8} {inQt:>8} {numActv:>7} {numRsrv:>7}'
-fmt = '{name:<25} {maxP:>9} {curP:>9} {numActv:>7} {numRsrv:>7}'
+fmt = '{name:<25} {maxP:>9} {curP:>9} {inQa:>8} {inQt:>8} {numActv:>7} {numRsrv:>7}'
+#fmt = '{name:<25} {maxP:>9} {curP:>9} {numActv:>7} {numRsrv:>7}'
 
-#inQa='InQ-actv',
-#inQt='InQ-tot', 
 hdrs = dict( name='Worker', 
              maxP='maxPcount', 
              curP='curPcount', 
+             inQa='InQ-actv',
+             inQt='InQ-tot', 
              numActv='NumActv', 
              numRsrv='NumRsrv'
              )
 print( fmt.format( **hdrs ) )
 
-totals = { 'name': 0,
-           'maxP': 0,
-           'curP': 0,
+totals = { 'name'   : 0,
+           'maxP'   : 0,
+           'curP'   : 0,
+           'inQa'   : 0,
+           'inQt'   : 0,
            'numActv': 0,
            'numRsrv': 0,
          }
@@ -62,8 +66,8 @@ for name in sorted( data.keys() ):
     stats = data[ name ][ 'STATS' ]
     max_procs = stats[ 'pool' ][ 'max-concurrency' ]
     cur_procs = len( stats[ 'pool' ][ 'processes' ] )
-#    inq_active = stats[ 'pool' ][ 'writes' ][ 'inqueues' ][ 'active' ]
-#    inq_total = stats[ 'pool' ][ 'writes' ][ 'inqueues' ][ 'total' ]
+    inq_active = stats[ 'pool' ][ 'writes' ][ 'inqueues' ][ 'active' ]
+    inq_total = stats[ 'pool' ][ 'writes' ][ 'inqueues' ][ 'total' ]
     numActive = len( data[ name ][ 'ACTIVELIST' ] )
     numReserved = len( data[ name ][ 'RESERVEDLIST' ] )
     if args.ignoreidle and numActive < 1 and numReserved < 1:
@@ -72,8 +76,8 @@ for name in sorted( data.keys() ):
         name=name,
         maxP=max_procs,
         curP=cur_procs,
-#        inQa=inq_active,
-#        inQt=inq_total,
+        inQa=inq_active,
+        inQt=inq_total,
         numActv=numActive,
         numRsrv=numReserved
         )
@@ -83,6 +87,8 @@ for name in sorted( data.keys() ):
     totals[ 'name' ] += 1
     totals[ 'maxP' ] += max_procs
     totals[ 'curP' ] += cur_procs
+    totals[ 'inQa' ] += inq_active
+    totals[ 'inQt' ] += inq_total
     totals[ 'numActv' ] += numActive
     totals[ 'numRsrv' ] += numReserved
 print( '\nTotals:' )
