b385fb8874e5a483100249cb7702b94114809ead
Andy Loftus <aloftus@gmail.com>
2016-04-12 12:50:03 -0500

    rename celeryconfig file

--- config/celeryconfig.py
+++ config/celeryconfig.py
@@ -2,15 +2,19 @@
 
 CELERY_ACCEPT_CONTENT = ['pickle']
 
+#CELERY_SEND_EVENTS = True
+
 #CELERYD_PREFETCH_MULTIPLIER = 4
 CELERYD_PREFETCH_MULTIPLIER = 1
+
+CELERYD_LOG_LEVEL = 'INFO'
 
 # Queues & Routes
 # Note: default queue is "celery"
 # Note: anything not explicitly defined below will go to the the default queue
 #CELERY_IMPORTS = ('psync', )
 #CELERY_CREATE_MISSING_QUEUES = True
-#CELERY_ROUTES = {
-#    'psync.sync_hardlink':   {'queue': 'hardlinks'},
-#}
+CELERY_ROUTES = {
+    'psync.sync_hardlink':   {'queue': 'hardlinks'},
+}
 ##    'psync.resync_dir_meta': {'queue': 'dirmeta'},
