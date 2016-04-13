d0807c9f23b4c6f8d1ac79c7b8ba5470d38586fa
Andy Loftus <aloftus@gmail.com>
2016-04-13 00:56:42 -0500

    Update for rabbitmq

--- scripts/discard_all.py
+++ scripts/discard_all.py
@@ -1,3 +1,13 @@
 import psync
 
-print( psync.app.control.discard_all() )
+# These might work IF the CELERY_ROUTES config setting were set properly
+#print( psync.app.control.discard_all() )
+#print( psync.app.control.purge() )
+
+# Otherwise, name the queues here
+import celery.bin.amqp
+amqp = celery.bin.amqp.amqp( app = psync.app )
+queuenames = [ 'celery', 'hardlinks', 'directories' ]
+for q in queuenames:
+    print( 'Queue: ' + q )
+    print( amqp.run( 'queue.purge', q ) )
