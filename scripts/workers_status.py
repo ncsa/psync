c7da38164b933b5db8fb01640af0c13ceb98d6b9
Andy Loftus <aloftus@gmail.com>
2016-04-13 00:36:23 -0500

    Added showqueues option

--- scripts/workers_status.py
+++ scripts/workers_status.py
@@ -19,6 +19,8 @@ parser.add_argument( '--ignoreidle', '-i', action='store_true',
     help='Show only workers with active tasks' )
 parser.add_argument( '--activedetails', '-d', action='store_true',
     help='Show details of active tasks' )
+parser.add_argument( '--showqueues', '-q', action='store_true',
+    help='Show queue listeners' )
 #parser.set_defaults( ignoreidle=False )
 args = parser.parse_args()
 
@@ -39,58 +41,66 @@ for worker, activelist in I.active().iteritems():
 for worker, reservedlist in I.reserved().iteritems():
     data[ worker][ 'RESERVEDLIST' ] = reservedlist
 
-# print summary
-fmt = '{name:<25} {maxP:>9} {curP:>9} {inQa:>8} {inQt:>8} {numActv:>7} {numRsrv:>7}'
-#fmt = '{name:<25} {maxP:>9} {curP:>9} {numActv:>7} {numRsrv:>7}'
+# get active_queues
+activequeuenames = []
+if args.listqueues:
+    for worker, qlist in I.active_queues().iteritems():
+        data[ worker ][ 'ACTIVEQUEUES' ] = qlist
+        for q in qlist:
+            activequeuenames.append( q[ 'name' ] )
+qnames = set( activequeuenames )
 
-hdrs = dict( name='Worker', 
-             maxP='maxPcount', 
-             curP='curPcount', 
-             inQa='InQ-actv',
-             inQt='InQ-tot', 
+# print summary
+fmt = '{name:<25} {maxP:>9} {curP:>9} {numActv:>7} {numRsrv:>7}'
+if args.listqueues:
+    for name in qnames:
+        fmt = fmt + ' {{{0}:>{1}}}'.format( name, len( name ) )
+
+hdrs = dict( name   ='Worker', 
+             maxP   ='maxPcount', 
+             curP   ='curPcount', 
              numActv='NumActv', 
              numRsrv='NumRsrv'
              )
+if args.listqueues:
+    hdrs.update( { k:k for k in qnames } )
 print( fmt.format( **hdrs ) )
 
 totals = { 'name'   : 0,
            'maxP'   : 0,
            'curP'   : 0,
-           'inQa'   : 0,
-           'inQt'   : 0,
            'numActv': 0,
            'numRsrv': 0,
          }
+if args.listqueues:
+    totals.update( { k:0 for k in qnames } )
 
 for name in sorted( data.keys() ):
     stats = data[ name ][ 'STATS' ]
-    max_procs = stats[ 'pool' ][ 'max-concurrency' ]
-    cur_procs = len( stats[ 'pool' ][ 'processes' ] )
-    inq_active = stats[ 'pool' ][ 'writes' ][ 'inqueues' ][ 'active' ]
-    inq_total = stats[ 'pool' ][ 'writes' ][ 'inqueues' ][ 'total' ]
-    numActive = len( data[ name ][ 'ACTIVELIST' ] )
-    numReserved = len( data[ name ][ 'RESERVEDLIST' ] )
-    if args.ignoreidle and numActive < 1 and numReserved < 1:
-        continue
-    print( fmt.format( 
-        name=name,
-        maxP=max_procs,
-        curP=cur_procs,
-        inQa=inq_active,
-        inQt=inq_total,
-        numActv=numActive,
-        numRsrv=numReserved
+    keys_vals = dict(  
+        name    = name,
+        maxP    = stats[ 'pool' ][ 'max-concurrency' ],
+        curP    = len( stats[ 'pool' ][ 'processes' ] ),
+        numActv = len( data[ name ][ 'ACTIVELIST' ] ),
+        numRsrv = len( data[ name ][ 'RESERVEDLIST' ] )
         )
-    )
+    if args.ignoreidle and keys_vals[ 'numActv' ] < 1 and keys_vals[ 'numRsrv' ] < 1:
+        continue
+    if args.listqueues:
+        activequeues = [ d['name'] for d in data[ name ][ 'ACTIVEQUEUES' ] ]
+        keys_vals.update( { k:'' for k in qnames } )
+        keys_vals.update( { k:'*' for k in qnames if k in activequeues } )
+    print( fmt.format( **keys_vals ) )
     if args.activedetails:
         print_active_details( data[ name ][ 'ACTIVELIST' ] )
-    totals[ 'name' ] += 1
-    totals[ 'maxP' ] += max_procs
-    totals[ 'curP' ] += cur_procs
-    totals[ 'inQa' ] += inq_active
-    totals[ 'inQt' ] += inq_total
-    totals[ 'numActv' ] += numActive
-    totals[ 'numRsrv' ] += numReserved
+    totals[ 'name' ]    += 1
+    totals[ 'maxP' ]    += keys_vals[ 'maxP' ]
+    totals[ 'curP' ]    += keys_vals[ 'curP' ]
+    totals[ 'numActv' ] += keys_vals[ 'numActv' ]
+    totals[ 'numRsrv' ] += keys_vals[ 'numRsrv' ]
+    if args.listqueues:
+        for qname in activequeues:
+            totals[ qname ] += 1
 print( '\nTotals:' )
 print( fmt.format( **hdrs ) )
 print( fmt.format( **totals ) )
